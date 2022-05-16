d340d8c7d3fd9131ef40224ba29835b8e5415673
Marcos Rempel <marcosdrempel02@gmail.com>
2022-05-15 23:28:20 -0300

    add y fix metodos mikasa

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -14,8 +14,9 @@ public class Juego extends InterfaceJuego {
 	private Image fondo;
 
 	public Juego() {
-		this.entorno = new Entorno(this, "Attack on Titan - Grupo 9", 800, 600);
-		this.mikasa = new Mikasa(entorno.ancho() / 2, entorno.alto() / 2, 3);
+		this.entorno = new Entorno(this, "Attack on Titan - Grupo 9", 1000, 1000);
+		// despues ponerlo en 800 x 600
+		this.mikasa = new Mikasa(entorno.ancho() / 2, entorno.alto() / 2, 3, 0);
 		this.kyojin = new Kyojin(entorno.ancho() / 2, entorno.alto() - 15, 2);
 		this.fondo = Herramientas.cargarImagen("pasto.jpg");
 		this.entorno.iniciar();
@@ -27,11 +28,15 @@ public class Juego extends InterfaceJuego {
 		kyojin.dibujar(entorno);
 		
 		if (entorno.estaPresionada('a')) {
-			mikasa.moverHaciaIzquierda();
-			//mikasa.acelerar();    SI USAMOS ACELERAR
+			mikasa.girar(-1);
 		}
+		
 		if (entorno.estaPresionada('d')) {
-			mikasa.moverHaciaDerecha();
+			mikasa.girar(1);
+		}
+		
+		if (entorno.estaPresionada('w')) {
+			mikasa.avanzar();
 			//mikasa.acelerar();    SI USAMOS ACELERAR
 		}
 
--- src/juego/Mikasa.java
+++ src/juego/Mikasa.java
@@ -9,33 +9,51 @@ public class Mikasa {
 	private double x;
 	private double y;
 	private double velocidad;
+	private double angulo;
 	private double ancho;
 	private double alto;
 	private Image img;
-	private Color color;
+	private Color color;      // esto es solo para el rectangulo que despues se termina ocultando, asi que da igual el color
 
-	public Mikasa(double x, double y, double velocidad) {
+	public Mikasa(double x, double y, double velocidad, double angulo) {
 		this.x = x;
 		this.y = y;
 		this.velocidad = velocidad;
-		this.ancho = 10;
-		this.alto = 10;
+		this.angulo = angulo;
+		this.ancho = 25;
+		this.alto = 115;
 		this.color = Color.YELLOW;
 		this.img = Herramientas.cargarImagen("mikasa (1).png");
 	}
 
 	public void dibujar(Entorno e) {
 		e.dibujarRectangulo(x, y, ancho, alto, 0, color);
-		e.dibujarImagen(img, this.x, this.y, 0);                //ultimo cambio (para que tenga imagen mikasa)
+		e.dibujarImagen(img, this.x, this.y, 0, 0.3);                //ultimo cambio (para que tenga imagen mikasa)
 	}
 
-	public void moverHaciaIzquierda() {
-		x -= velocidad;
+	public void girar(double modificador) {
+		angulo = angulo + modificador;
+		if (angulo > Math.PI * 2) {
+			angulo = angulo - Math.PI * 2;
+		}
+		if (angulo < 0) {
+			angulo = angulo + Math.PI * 2;
+		}
 	}
 
-	public void moverHaciaDerecha() {
-		x += velocidad;
+	public void avanzar() {
+		x = x + Math.cos(angulo) * 2;
+		y = y + Math.sin(angulo) * 2;
 	}
+	
+	/* TODO
+	public boolean chocasteConObstaculo() {
+		
+	}
+	
+	public boolean chocasteConEntorno() {
+		
+	} */
 
 	public double getX() {
 		return x;
